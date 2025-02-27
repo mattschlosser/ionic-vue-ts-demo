0418b6ef224190d646b492473a0c4509e12b5894
Matt Schlosser <mattschlosser@users.noreply.github.com>
2025-02-27 11:25:48 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <ion-app >
+    <ion-page >
+      <ion-header>
+        <ion-toolbar>
+          <ion-title size="large">
+                <!--   ^? -->
+            Hello World
+          </ion-title>
+        </ion-toolbar>
+      </ion-header>
+    </ion-page>
+  </ion-app>
+</template>
+
+<script setup lang="ts">
+import { IonApp, IonPage, IonHeader, IonToolbar, IonTitle } from '@ionic/vue';
+</script>
